5269375bbd0ac01bed4973cbf45b8526b2d91e3b
cma013 <cma013@fiu.edu>
2022-02-01 11:19:35 -0500

    reformat

--- EventListApp/index.js
+++ EventListApp/index.js
@@ -3,7 +3,15 @@ import { api } from "./api.js";
 
 // get all events
 let data = api.getEvents().then((data) => {
-    console.log(data[0])
+    console.log('get')
+    let checkUpdate = (newEvent, data) =>{
+        for (let key in newEvent) {
+            console.log(data)
+            if (key in data) {
+                data[key] = newEvent[key];
+            }
+        }
+    }
     for(let i=0; i<data.length; i++){
         let tr = document.createElement('TR');
 
@@ -11,15 +19,10 @@ let data = api.getEvents().then((data) => {
         let input1 = document.createElement('INPUT')
         input1.value = data[i]['eventName']
         input1.setAttribute('disabled', 'True')
-        //input1.setAttribute("onchange", "inputChange(this.value)")
+
         input1.addEventListener('change', (event) => {
-            newEvent['eventName'] = event.target.value
-            for (let key in newEvent) {
-                console.log(data[i])
-                if (key in data[i]) {
-                    data[i][key] = newEvent[key];
-                }
-            }
+            newEvent['eventName'] = event.target.value;
+            checkUpdate(newEvent, data[i])
             console.log(newEvent)
         }, false)
         
@@ -29,19 +32,15 @@ let data = api.getEvents().then((data) => {
 
         let td2 = document.createElement("TD");
         let input2 = document.createElement('INPUT');
+
+        // q: how to use getTime()?
         input2.value = data[i]['startDate'];
         input2.setAttribute('disabled', 'True');
-        //input2.setAttribute("onchange", "inputChange(this.value)")
+       
         input2.addEventListener('change', (event) => {
-            newEvent['startDate'] = event.target.value
-            for (let key in newEvent) {
-                console.log(key)
-                if (key in data[i]) {
-
-                    data[i][key] = newEvent[key];
-                }
-            }
-            console.log(newEvent)
+            newEvent['startDate'] = event.target.value;
+            checkUpdate(newEvent, data[i]);
+            console.log(newEvent);
         }, false)
         
         td2.appendChild(input2);
@@ -51,26 +50,14 @@ let data = api.getEvents().then((data) => {
         let input3 = document.createElement('INPUT');
         input3.value = data[i]['endDate'];
         input3.setAttribute('disabled', 'True');
-        //input3.setAttribute("onchange", "inputChange(this.value)")
+        
+
         input3.addEventListener('change', (event) => {
-            newEvent['endDate'] = event.target.value
-            for (let key in newEvent) {
-                console.log(key)
-                if (key in data[i]) {
-
-                    data[i][key] = newEvent[key];
-                }
-                console.log(data[i])
-            }
+            newEvent['endDate'] = event.target.value;
+            checkUpdate(newEvent, data[i]);
             console.log(newEvent)
-        }, false)
-        for (let key in newEvent) {
-            console.log(key)
-            if (key in data[i]) {
-
-                data[i][key] = newEvent[key];
-            }
-        }
+        }, false);
+        
         td3.appendChild(input3);
         tr.appendChild(td3);
 
@@ -95,81 +82,59 @@ let data = api.getEvents().then((data) => {
             input2.removeAttribute('disabled', 'True');
             input3.removeAttribute('disabled', 'True');
             
-            console.log(btn1.innerHTML)
-            btn1.innerHTML = 'SAVE'
-            console.log(data[i])
-            console.log(newEvent)
-            for (let key in newEvent) {
-                console.log(key)
-                if (key in data[i]) {
-                    
-                    data[i][key] = newEvent[key];
-                }
-            }
-            console.log(data[i])
+            btn1.innerHTML = 'SAVE';
             btn1.onclick = function() {
-                
-                for(let key in newEvent){
-                    if (key in data[i]){
-                        data[i][key] = newEvent[key];
-                    } 
-                }
-                
                 api.updateEvent(data[i]['id'], data[i])
-            }
-            //
-        }
+            };
+        };
 
         btn2.onclick = function () {
             var div = this.parentElement.parentElement;
-            console.log(div)
             div.style.display = "none";
-            api.deleteEvent(data[i]['id'])
-        }
-    
+            api.deleteEvent(data[i]['id']);
+        };
         document.getElementsByTagName('TABLE')[0].appendChild(tr)
-    }
-})   
+    };
+});
 
 // add new event
-let newEvent = {}
+let newEvent = {};
 var addbtn = document.getElementsByClassName("addbtn");
 addbtn[0].addEventListener('click',() =>{
     let tr = document.createElement('TR');
     let td1 = document.createElement("TD");
-    let input1 = document.createElement('INPUT')
-    //input1.setAttribute("onchange", "inputChange(this.value)")
+    let input1 = document.createElement('INPUT');
     input1.addEventListener('change', (event)=>{
         newEvent['eventName'] = event.target.value
         console.log(newEvent)
-    },false)
+    },false);
  
-    td1.appendChild(input1)
-    tr.appendChild(td1)
+    td1.appendChild(input1);
+    tr.appendChild(td1);
 
     let td2 = document.createElement("TD");
     let input2 = document.createElement('INPUT');
-    //input2.setAttribute("onchange", "inputChange(this.value)")
+    input2.setAttribute('type', 'date');
     input2.addEventListener('change', (event) => {
         newEvent['startDate'] = event.target.value
         console.log(newEvent)
-    }, false)
+    }, false);
   
     td2.appendChild(input2);
     tr.appendChild(td2);
 
     let td3 = document.createElement("TD");
     let input3 = document.createElement('INPUT');
-    //input3.setAttribute("onchange", "inputChange(this.value)")
+    input3.setAttribute('type', 'date');
     input3.addEventListener('change', (event) => {
         newEvent['endDate'] = event.target.value
         console.log(newEvent)
-    }, false)
+    }, false);
 
     td3.appendChild(input3);
     tr.appendChild(td3);
 
-    let td4 = document.createElement('TD')
+    let td4 = document.createElement('TD');
     var btn1 = document.createElement("button");
     btn1.className = 'save';
     btn1.appendChild(document.createTextNode("SAVE"));
@@ -179,38 +144,29 @@ addbtn[0].addEventListener('click',() =>{
 
     td4.appendChild(btn1);
     td4.appendChild(btn2);
-    tr.appendChild(td4)
+    tr.appendChild(td4);
 
     td1.className = 'event-name';
     td2.className = 'start-date';
     td2.className = 'end-date';
 
+    // save event
     btn1.onclick = function() {
         console.log()
         newEvent['id'] = document.getElementsByTagName('TR').length /2 +1;
         console.log(newEvent)
         api.addEvent(newEvent)
-    }
+    };
 
+    // delete event
     btn2.onclick = function () {
         var div = this.parentElement.parentElement;
         console.log(div)
         div.style.display = "none";
         api.deleteEvent(data[i]['id'])
-    }
+    };
 
     document.getElementsByTagName('TABLE')[0].appendChild(tr)
 }, false)
 
 
-// save event
-var savebtn = document.getElementsByClassName("addbtn");
-savebtn[0].addEventListener('click', ()=>{
-    const selectElement = document.querySelector('input');
-    let result = [];
-    selectElement.addEventListener('change', (event) => {
-        
-        result.push(event);
-    });
-    console.log(result)
-}, false)
